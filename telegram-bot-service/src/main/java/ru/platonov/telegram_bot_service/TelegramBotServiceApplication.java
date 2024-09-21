29b9355949ecbfc7a279f5ce7e3a95c6d575acb4
Daniil <d_platonov@internet.ru>
2024-09-22 01:35:53 +0300

    Initial commit with .gitignore

---
+++ telegram-bot-service/src/main/java/ru/platonov/telegram_bot_service/TelegramBotServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.platonov.telegram_bot_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramBotServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramBotServiceApplication.class, args);
+	}
+
+}
